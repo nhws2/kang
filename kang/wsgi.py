b14b8373f70a2962834b56d6c4e7308dcb062d28
nhws2 <nhw_s2@naver.com>
2019-07-12 19:16:21 +0900

    first

---
+++ kang/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kang project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kang.settings')
+
+application = get_wsgi_application()
